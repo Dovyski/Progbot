12183b9ff67daf696a0192c7dee3f4ce6f7278f0
Fernando Bevilacqua <dovyski@gmail.com>
2013-06-24 16:42:16 -0300

    Add some layout love to reviews page.

--- reviews.php
+++ reviews.php
@@ -10,64 +10,83 @@
 	echo '<div class="hero-unit">';
 		echo '<h1>Reviews</h1>';
 		echo '<p>Revisão de trabalhos.</p>';
-	echo '</div>';		
+	echo '</div>';
 	
 	if ($aChallengeId != 0) {
 		// Showing all answers of a challenge
-		$aAnswers = challengeFindAnswersById($aChallengeId);
+		$aAnswers 	= challengeFindAnswersById($aChallengeId);
+		$aChallenge = challengeGetById($aChallengeId);
 		
 		echo '<div class="row">';
-			echo '<div class="span12">';
-					echo '<h1>Respostas até o momento</h1>';
-				echo '</div>';
+			echo '<div class="span4">';
+				echo '<a href="reviews.php"><i class="icon-backward"></i> Voltar</a><br/><br/>';				
+			echo '</div>';
 		echo '</div>';
 		
-		if (count($aAnswers) == 0) {
-			echo '<div class="row">';
-				echo '<div class="span12">';
-						echo '<p>Não há respostas para esse desafio.</p>';
-					echo '</div>';
-			echo '</div>';
-		} else {
-			foreach($aAnswers as $aIdProgram => $aRow) {
-				echo '<div class="row">';
-					echo '<div class="span12">';
-							echo '<h2><a href="code.php?challenge='.$aChallengeId.'&user='.$aRow['fk_user'].'">User_id='.$aRow['fk_user'].'</a> <span class="label label-warning">Nota '.$aRow['grade'].'</span></h2>';
-						echo '</div>';
-				echo '</div>';
+		echo '<div class="bloco-desafios"><h4><i class="icon-eye-open"></i> Respostas para desafio "'.$aChallenge['name'].'"</h4></div>';
+		
+		echo '<div class="bloco-desafios">';
+			if (count($aAnswers) == 0) {
+				echo '<p>Não há respostas para esse desafio ainda.</p>';
+				
+			} else {
+				echo '<table class="table table-hover">';
+					echo '<thead>';
+						echo '<th style="width: 5%;"></th>';
+						echo '<th>Aluno</th>';
+						echo '<th>Última atualização</th>';
+						echo '<th>Nota</th>';
+						echo '<th>Opções</th>';
+					echo '</thead>';
+					echo '<tbody>';
+						foreach($aAnswers as $aIdProgram => $aRow) {
+							echo '<tr>';
+								echo '<td><i class="icon-file"></i></td>';
+								echo '<td>';
+									// TODO: FIX THIS! Use batch select to get user info
+									$aUser = userGetById($aRow['fk_user']);
+									echo '<a href="user.php?id='.$aRow['fk_user'].'">'.$aUser['name'].'</a>';
+								echo '</td>';
+								echo '<td>'.date('d/m/Y H:i:s', $aRow['last_update']).'</td>';
+								echo '<td>'.($aRow['grade'] < 0 ? 'N/A' : $aRow['grade']).'</td>';
+								echo '<td><a href="code.php?challenge='.$aChallengeId.'&user='.$aRow['fk_user'].'"><i class="icon-zoom-in"></i>Revisar</a></td>';
+							echo '</tr>';
+						}
+					echo '</tbody>';
+				echo '</table>';
 			}
-		}
+			//echo '<div class="bloco-desafios-legenda">Desafios já resolvidos</div>';
+		echo '</div>';
 		
 	} else {
 		// Showing all challenges of user's group
 		$aChallenges = challengeFindByGroup($aUserInfo['fk_group']);
 		
-		echo '<div class="row">';
-			echo '<div class="span12">';
-					echo '<h1>Desafios do seu grupo</h1>';
-				echo '</div>';
-		echo '</div>';
+		echo '<div class="bloco-desafios"><h4><i class="icon-eye-open"></i> Desafios disponíveis para revisão</h4></div>';
 		
-		if (count($aChallenges) == 0) {
-			echo '<div class="row">';
-				echo '<div class="span12">';
-						echo '<p>Não há desafios ativos para o seu grupo.</p>';
-					echo '</div>';
-			echo '</div>';
-		} else {
-			foreach($aChallenges as $aIdChallenge => $aRow) {
-				echo '<div class="row">';
-					echo '<div class="span12">';
-							echo '<h2><a href="reviews.php?challenge='.$aIdChallenge.'">'.$aRow['name'].'</a> <span class="label label-warning">Nível '.$aRow['level'].'</span></h2>';
-						echo '</div>';
-				echo '</div>';
+		echo '<div class="bloco-desafios">';
+			if (count($aChallenges) == 0) {
+				echo '<p>Não há desafios ativos para o seu grupo.</p>';
+				
+			} else {
+				echo '<table class="table table-hover">';
+					echo '<tbody>';
+						foreach($aChallenges as $aIdChallenge => $aRow) {
+							echo '<tr>';
+								echo '<td style="width: 5%;"><i class="icon-list-alt"></i></td>';
+								echo '<td>';
+									echo '<a href="reviews.php?challenge='.$aIdChallenge.'">'.$aRow['name'].'</a> ';
+									echo '<span class = "label label-warning" > '.challengeLevelToString($aRow['level']).' </span> '; // TODO: create some standart way to print challenges.
+									//echo '<p>'.$aRow['description'].'</p>';
+								echo '</td>';
+							echo '</tr>';
+						}
+					echo '</tbody>';
+				echo '</table>';
 			}
-		}
+			//echo '<div class="bloco-desafios-legenda">Desafios já resolvidos</div>';
+		echo '</div>';
 	}
-	
-	
-	
-	
 	
 	layoutFooter();
 ?>
