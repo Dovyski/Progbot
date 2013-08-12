0e89682ea2c4a8f5ea49c0821075ae4b31f12df5
Fernando Bevilacqua <dovyski@gmail.com>
2013-08-12 17:12:19 -0300

    login.php MVC'd.

--- login.php
+++ login.php
@@ -42,37 +42,7 @@
 		}
 	}
 	
-	layoutHeader('Login');
-	
-	echo '<div class="hero-unit fundo-icone icone-password">';
-		echo '<h1>Login</h1>';
-		echo '<p>Você precisa efetuar login para continuar.</p>';
-	echo '</div>';
-	
-	echo '<div class="row">';
-		echo '<div class="span12">';
-			echo '<form class="form-horizontal" action="login.php" method="post">
-			        <fieldset>
-			          <legend>Informações de login</legend>
-			          <div class="control-group '.($aLoginError ? 'error' : '').'">
-			            <label class="control-label">CPF</label>
-			            <div class="controls docs-input-sizes">
-			              <input name="user" class="span3" type="text" placeholder="Informe seu CPF">
-			            </div>
-			            <label class="control-label">Senha do Moodle</label>
-			            <div class="controls docs-input-sizes">
-			              <input name="password" class="span3" type="password" placeholder="Sua senha usada no Moodle">
-						  '.($aLoginError ? '<span class="help-inline">Usuário ou password inválidos.</span>' : '').'
-			            </div>
-			          </div>
-			          <div class="form-actions">
-			            <button type="submit" class="btn btn-primary">Entrar</button>
-			            <button type="reset" class="btn">Limpar</button>
-			          </div>
-			        </fieldset>
-			      </form>'; 
-		echo '</div>';
-	echo '</div>';
-	
-	layoutFooter();
+	View::render('login', array(
+		'loginError' => $aLoginError
+	));
 ?>
---
+++ view/html/login.php
@@ -0,0 +1,39 @@
+<?php 
+	require_once dirname(__FILE__).'/layout.php';
+	
+	layoutHeader('Login', View::baseUrl());
+	
+	$aData = View::data();
+	
+	echo '<div class="hero-unit fundo-icone icone-password">';
+		echo '<h1>Login</h1>';
+		echo '<p>Você precisa efetuar login para continuar.</p>';
+	echo '</div>';
+	
+	echo '<div class="row">';
+		echo '<div class="span12">';
+			echo '<form class="form-horizontal" action="login.php" method="post">
+			        <fieldset>
+			          <legend>Informações de login</legend>
+			          <div class="control-group '.($aData['loginError'] ? 'error' : '').'">
+			            <label class="control-label">CPF</label>
+			            <div class="controls docs-input-sizes">
+			              <input name="user" class="span3" type="text" placeholder="Informe seu CPF">
+			            </div>
+			            <label class="control-label">Senha do Moodle</label>
+			            <div class="controls docs-input-sizes">
+			              <input name="password" class="span3" type="password" placeholder="Sua senha usada no Moodle">
+						  '.($aData['loginError'] ? '<span class="help-inline">Usuário ou password inválidos.</span>' : '').'
+			            </div>
+			          </div>
+			          <div class="form-actions">
+			            <button type="submit" class="btn btn-primary">Entrar</button>
+			            <button type="reset" class="btn">Limpar</button>
+			          </div>
+			        </fieldset>
+			      </form>'; 
+		echo '</div>';
+	echo '</div>';
+	
+	layoutFooter();
+?>
