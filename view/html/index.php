8e41fa204349d640b9ab489b6496991abda7a908
Fernando Bevilacqua <dovyski@gmail.com>
2013-08-13 10:51:28 -0300

    Add some layout love to index.php

--- view/html/index.php
+++ view/html/index.php
@@ -5,24 +5,24 @@
 	
 	echo '<div class="hero-unit">';
 		echo '<h1>Codebot</h1>';
-		echo '<p>Texto bem bonito falando brevemente sobre o sistema</p>';
-		echo '<p><a href="oops.php" class="btn btn-primary btn-large">Saber mais &raquo;</a></p>';
+		echo '<p>Ensine e aprenda programação. Ponto.</p>';
+		//echo '<p><a href="oops.php" class="btn btn-primary btn-large">Saber mais &raquo;</a></p>';
 	echo '</div>';
 	
 	echo '<div class="row">';
-		echo '<div class="span4">';
-			echo '<h2>Guia rápido <span class="label label-warning">Novo</span></h2>';
-			echo '<p>O NCC possui diversas vantagens e algumas obrigações para seus usuários. Aprenda todas elas rapidamente através dos 8 mandamentos do NCC, um guia simples e rápido de ler.</p>';
+		echo '<div class="span4 index-hero1">';
+			echo '<h2>Programe no Browser</h2>';
+			echo '<p>Um ambiente de desenvolvimento completo, disponível através do browser: editor, terminal, compilação, o que for necessário.</p>';
 		echo '</div>';
 		
-		echo '<div class="span4">';
-			echo '<h2>Armazenamento</h2>';
-			echo '<p>Cada usuário do NCC possui um espaço reservado no servidor para guardar arquivos (documentos, trabalhos, etc). Saiba como usar esse espaço para guardar e proteger seus dados.</p>';
+		echo '<div class="span4 index-hero2">';
+			echo '<h2>Desafios e Tarefas</h2>';
+			echo '<p>Resolva desafios categorizados por assunto e nível de dificuldade. Professores podem criar desafios novos a qualquer momento.</p>';
 		echo '</div>';
 		
-		echo '<div class="span4">';
-			echo '<h2>Contato</h2>';
-			echo '<p>Não encontrou aquilo que procurava, está com dúvidas sobre o funcionamento do NCC, tem críticas ou sugestões? Fale conosco: <a href="mailto:ncc@uffs.edu.br">ncc@uffs.edu.br</a></p>';
+		echo '<div class="span4 index-hero3">';
+			echo '<h2>Feedback</h2>';
+			echo '<p>Código e feedback dado pelo professor são tratados de forma integrada: feedback junto ao código, porém sem modificá-lo.</p>';
 		echo '</div>';
 	echo '</div>';
 	
