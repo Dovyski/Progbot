92f7a0f419e2dba57e66c4deace74687e88b6481
Fernando Bevilacqua <dovyski@gmail.com>
2015-05-13 14:58:04 -0300

    A bit of love to the review page.

--- reviews.php
+++ reviews.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 	require_once dirname(__FILE__).'/inc/globals.php';
-	
+
 	authAllowAuthenticated();
-	
+
 	$aUser = userGetById($_SESSION['user']['id']);
-	
+
 	if (!userIsLevel($aUser, USER_LEVEL_PROFESSOR)) {
 		header("Location: restricted.php");
 		exit();
@@ -13,19 +13,20 @@
 	$aData					= array();
 	$aChallengeId 			= isset($_GET['challenge']) ? $_GET['challenge'] : 0;
 	$aUserInfo				= userGetById($_SESSION['user']['id']);
-	
+
 	if ($aChallengeId != 0) {
-		// Showing all answers of a challenge		
+		// Showing all answers of a challenge
 		$aData['challenge'] = challengeGetById($aChallengeId);
 		$aData['answers'] 	= challengeFindAnswersById($aChallengeId);
-		
+
 	} else {
 		// Showing all challenges of user's group
 		$aData['challenges'] = challengeFindByGroup($aUserInfo['fk_group']);
+		$aData['group'] = groupGetById($aUserInfo['fk_group']);
 	}
-	
+
 	$aData['challengeId']	= $aChallengeId;
 	$aData['user']	 		= $aUserInfo;
-	
+
 	View::render('reviews', $aData);
 ?>
--- view/html/reviews.php
+++ view/html/reviews.php
@@ -1,38 +1,38 @@
-<?php 
+<?php
 	require_once dirname(__FILE__).'/layout.php';
 
 	layoutHeader('Revisões', View::baseUrl());
-	
+
 	$aData				= View::data();
 	$aChallengeId 		= $aData['challengeId'];
 	$aUserInfo	 		= $aData['user'];
-	
+
 	echo '<div class="jumbotron">';
 		echo '<div class="container">';
 			echo '<h1>Revisão</h1>';
 			echo '<p>Avaliação das respostas enviadas pelos alunos.</p>';
 		echo '</div>';
 	echo '</div>';
-	
+
 	echo '<div class="container">';
-	
+
 	if ($aChallengeId != 0) {
 		// Showing all answers of a challenge
 		$aAnswers 	= $aData['answers'];
 		$aChallenge = $aData['challenge'];
-		
+
 		echo '<div class="row">';
-			echo '<div class="span4">';
-				echo '<a href="reviews.php"><i class="fa fa-arrow-circle-left"></i> Voltar</a><br/><br/>';				
+			echo '<div class="col-md-4">';
+				echo '<a href="reviews.php"><i class="fa fa-arrow-circle-left"></i> Voltar</a><br/><br/>';
 			echo '</div>';
 		echo '</div>';
-		
+
 		echo '<div class="panel panel-default">';
 			echo '<div class="panel-heading">Respostas para "'.$aChallenge['name'].'"</em></div>';
 			echo '<div class="bloco-desafios">';
 				if (count($aAnswers) == 0) {
 					echo '<p>Não há respostas para esse desafio ainda.</p>';
-					
+
 				} else {
 					echo '<table class="table table-hover">';
 						echo '<thead>';
@@ -45,11 +45,11 @@
 						echo '<tbody>';
 							foreach($aAnswers as $aIdProgram => $aRow) {
 								echo '<tr>';
-									echo '<td><i class="fa fa-'.(rand(0, 10) < 5 ? 'circle-o' : 'check-circle').'"></i></td>';
+									echo '<td><i class="fa fa-'.($aRow['grade'] < 0 ? 'circle-o' : 'check-circle').'"></i></td>';
 									echo '<td>';
 										// TODO: FIX THIS! Use batch select to get user info
 										$aUser = userGetById($aRow['fk_user']);
-										echo '<a href="user.php?id='.$aRow['fk_user'].'">'.$aUser['name'].'</a>';
+										echo View::escape($aUser['name']);
 									echo '</td>';
 									echo '<td>'.date('d/m/Y H:i:s', $aRow['last_update']).'</td>';
 									echo '<td>'.($aRow['grade'] < 0 ? 'N/A' : $aRow['grade']).'</td>';
@@ -62,22 +62,23 @@
 				//echo '<div class="bloco-desafios-legenda">Desafios já resolvidos</div>';
 			echo '</div>';
 		echo '</div>';
-		
+
 	} else {
 		// Showing all challenges of user's group
 		$aChallenges = $aData['challenges'];
-		
+		$aGroup = $aData['group'];
+
 		echo '<div class="panel panel-default">';
-			echo '<div class="panel-heading">Grupo Algoritmos 2014.2N</div>';
+			echo '<div class="panel-heading"><i class="fa fa-group"></i> '.(isset($aGroup['name']) ? View::escape($aGroup['name']) : '?').'</div>';
 			echo '<div class="bloco-desafios">';
 				if (count($aChallenges) == 0) {
 					echo '<p>Não há desafios ativos para o seu grupo.</p>';
-					
+
 				} else {
 					echo '<table class="table table-hover">';
 						echo '<thead>';
 							echo '<th style="width: 1%;"></th>';
-							echo '<th style="width: 80%;">Título</th>';
+							echo '<th style="width: 80%;">Desafio</th>';
 							echo '<th>Respostas</th>';
 						echo '</thead>';
 						echo '<tbody>';
@@ -97,8 +98,8 @@
 			echo '</div>';
 		echo '</div>';
 	}
-	
+
 	echo '</div>';
-	
+
 	layoutFooter(View::baseUrl());
 ?>
